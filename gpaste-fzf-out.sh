8f49e3cddeb709b65ddd8adaed6b6dd3b51000bf
Govindarajulu Varadarajan <govind.varadar@gmail.com>
2025-11-17 17:31:56 -0800

    gpaste-fzf-out.sh: Add action for delete key
    Signed-off-by: Govindarajulu Varadarajan <govind.varadar@gmail.com>

--- gpaste-fzf-out.sh
+++ gpaste-fzf-out.sh
@@ -9,5 +9,6 @@ kitty bash -c "$GPASTE_FZF_OUT --read0 | fzf --reverse \
 	--with-nth=5.. \
 	--bind 'enter:become(setsid -f nohup gpaste-client select {4} 2>&1 > /dev/null)' \
 	--bind 'ctrl-d:reload(gpaste-client delete {4} ; $GPASTE_FZF_OUT --read0)' \
+	--bind 'delete:reload(gpaste-client delete {4} ; $GPASTE_FZF_OUT --read0)' \
 	--bind 'ctrl-r:reload($GPASTE_FZF_OUT --read0)' \
 	"
